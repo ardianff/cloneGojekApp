b031bbe4bd0115a750e1c4a771df09f4cce3bb08
ardianff <ardianfirmansyah123@gmail.com>
2020-07-12 17:53:10 +0700

    add

---
+++ App.js
@@ -0,0 +1,40 @@
+import React,{Component} from 'react';
+import { Platform, StyleSheet, Text, View, Image} from 'react-native';
+
+export default class App extends Component{
+  render() {
+    return(
+      <View style={{flex: 1}}>
+        <View style={{flex:1,backgroundColor:'pink'}}>
+          <Text>Clone Gojek App</Text>
+        </View>
+        <View style={{height:54,flexDirection:'row',backgroundColor:'white'}}>
+          <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/home-active.png')}/>
+            <Text style={{fontSize: 10, color: '#545454', marginTop:4,color:'#43AB4A'}}>Home</Text>
+          </View>
+          <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/order.png')}/>
+            <Text style={{fontSize: 10, color: '#545454', marginTop:4}}>Orders</Text>
+          </View>
+          <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/help.png')}/>
+            <Text style={{fontSize: 10, color: '#545454', marginTop:4}}>Help</Text>
+          </View>
+          <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/inbox.png')}/>
+            <Text style={{fontSize: 10, color: '#545454', marginTop:4}}>Inbox</Text>
+          </View>
+          <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/account.png')}/>
+            <Text style={{fontSize: 10, color: '#545454', marginTop:4}}>Account</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
