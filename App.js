43a9cc1680ab2de50066c7f515dea456c301a825
ardianff <ardianfirmansyah123@gmail.com>
2020-07-14 19:24:38 +0700

    update go news

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react';
-import { Platform, StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, TextInput, ScrollView, Button, TouchableOpacity} from 'react-native';
 
 export default class App extends Component{
   render() {
     return(
       <View style={{flex: 1}}>
-        <View style={{flex:1,backgroundColor:'white'}}>
+        <ScrollView style={{flex:1,backgroundColor:'white'}}>
           {/* search bar */}
           <View style={{marginHorizontal:17, flexDirection:'row', paddingTop:15}}>
             <View style={{position:"relative", flex:1}}>
@@ -96,7 +96,25 @@ export default class App extends Component{
               </View>
             </View>
           </View>
-        </View>
+          <View style={{height:17,backgroundColor:'#F2F2F4', marginTop:20}}></View>
+          {/* New Section */}
+          <View style={{paddingTop:16, paddingHorizontal:16}}>
+            <View style={{position: 'relative',}}>
+              <Image source={require('./dummy/sepak-bola.jpg')} style={{height:200, width:'100%',borderRadius:6}}/>
+              <View style={{width:'100%', height:'100%', position:"absolute", top: 0 , left:0, backgroundColor:'black', opacity:0.2, borderRadius:6}}></View>
+              <View style={{height:15, width:55, position:"absolute",top: 16, left:16}}>
+                <Image source={require('./logo/white.png')} style={{width: undefined,height: undefined, resizeMode:"contain", flex: 1}} /> 
+              </View>
+              </View>
+            <View style={{paddingTop:16, paddingBottom:20, borderBottomColor: '#E8E9ED', borderBottomWidth:1, marginBottom:20}}>
+              <Text style={{fontSize:16, fontWeight:"bold",color:'#1C1C1C'}}>GO-NEWS</Text>
+              <Text style={{fontSize:14, fontWeight:"normal",color:'#7A7A7A', marginBottom:11}}>Dimas Drajat selamatkan penalti, Timnas U-23 kalahkan Brunei</Text>
+              <TouchableOpacity style={{backgroundColor: '#61A756', paddingHorizontal: 12, paddingVertical:11, alignSelf:"flex-end", borderRadius:4}}>
+                <Text style={{fontSize:13, fontWeight:"bold", color:"white", textAlign:"center"}}>READ</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
         <View style={{height:54,flexDirection:'row',backgroundColor:'white'}}>
           <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
             <Image style={{width: 26, height: 26}} source={require('./icon/home-active.png')}/>
